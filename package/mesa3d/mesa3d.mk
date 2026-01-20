6c432306275c038342b0b7cac1f8cd4703316d0b
Cnuuyy <90187830+egor835@users.noreply.github.com>
2026-01-20 22:53:08 +0300

    Update mesa3d.mk

--- package/mesa3d/mesa3d.mk
+++ package/mesa3d/mesa3d.mk
@@ -26,6 +26,12 @@ MESA3D_DEPENDENCIES = \
 # Disable assembly usage.
 MESA3D_CONF_OPTS = --disable-asm
 
+# Disable TLS
+MESA3D_CONF_OPTS += --disable-elf-tls \
+                    --disable-glx-tls 
+#                    --disable-shared-glapi \
+#                    --disable-asm
+
 # Disable static, otherwise configure will fail with: "Cannot enable both static
 # and shared."
 ifeq ($(BR2_SHARED_STATIC_LIBS),y)
